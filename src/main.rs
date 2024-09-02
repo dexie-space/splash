e53d40a66453f2cd328b35bfd32e8615b0032b88
dennis <dns@dexie.space>
2024-09-02 20:21:00 +0200

    update libp2p to 0.54

--- src/main.rs
+++ src/main.rs
@@ -104,11 +104,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
             )?;
 
             // Create a Kademlia behaviour.
-            let mut cfg = kad::Config::default();
-
-            cfg.set_protocol_names(vec![StreamProtocol::try_from_owned(
-                "/splash/kad/1".to_string(),
-            )?]);
+            let mut cfg = kad::Config::new(
+                StreamProtocol::try_from_owned(
+                    "/splash/kad/1".to_string(),
+                ).expect("protocol name is valid")
+            );
 
             cfg.set_query_timeout(Duration::from_secs(60));
             let store = kad::store::MemoryStore::new(key.public().to_peer_id());
@@ -250,7 +250,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
                         }
                     }
                 },
-                SwarmEvent::Behaviour(SplashBehaviourEvent::Identify(identify::Event::Received { info: identify::Info { observed_addr, listen_addrs, .. }, peer_id })) => {
+                SwarmEvent::Behaviour(SplashBehaviourEvent::Identify(identify::Event::Received {
+                    info: identify::Info { observed_addr, listen_addrs, .. },
+                    peer_id,
+                    connection_id: _
+                })) => {
                     for addr in listen_addrs {
                         swarm.behaviour_mut().kademlia.add_address(&peer_id, addr);
                     }
